8c42f6407d88faeb8170cc216f38b451eed26c65
Fridolin Linder <fridolin.linder@gmail.com>
2017-05-07 11:45:22 -0400

    New active learning experiment
    - Varying sparsity of the data
    - Stemming instead of (non funcitonal) lemmatization

--- experiments/active_vs_random.py
+++ experiments/active_vs_random.py
@@ -1,12 +1,12 @@
 import csv
-import spacy 
 import re
 import logging
 import sys
 import itertools
-
 import pandas as pd
 import numpy as np
+import Stemmer
+import time
 
 from gensim import corpora, matutils
 from sklearn.linear_model import SGDClassifier
@@ -36,7 +36,7 @@ def make_dictionaries(documents, parser):
 
     for d in documents:
         # Get unigrams (lemmas)
-        unis = [token.lemma_ for token in parser(d)] 
+        unis = [t for t in parser(d)] 
         # Get bigrams
         bis = bi_from_uni(unis)
         # update dictionaries
@@ -56,7 +56,7 @@ def label_iter(input_file_name):
 
 def make_ngrams(tweet_text, parser, unigrams, bigrams):
     # Tokenize and make bigrams
-    unis = [token.lemma_ for token in parser(tweet_text)] 
+    unis = [t for t in parser(tweet_text)] 
     bis = bi_from_uni(unis) # Get bow representation for dictionary terms
     uni_bow = unigrams.doc2bow(unis)
     bi_bow = bigrams.doc2bow(bis)
@@ -69,45 +69,90 @@ def get_metrics(y_true, y_pred):
     rec = recall_score(y_true, y_pred)
     return [prec, rec, f1]
 
-def iteration(i):
-    print(f'iteration {i}: random annotation')
+
+def balance_data(X, dat, balance):
+    
+    # Get index of negative and postive rows
+    p_max = len(dat.index[dat.annotation == 1])
+    n_max = len(dat.index[dat.annotation == 0])
+    
+    # Get the balance in the data
+    original_balance = p_max / (n_max + p_max) 
+    
+    # Pick the data
+    if balance >= original_balance:
+        # Use all positives and sample negatives
+        n_neg = p_max * (1-balance) // balance
+        neg_idx = dat[dat.annotation == 0].sample(n_neg).index
+        pos_idx = dat[dat.annotation == 1].index
+    else:
+        # Use all negatives and sample positives
+        n_pos = balance * n_max // (1 - balance)  
+        neg_idx = dat[dat.annotation == 0].index
+        pos_idx = dat[dat.annotation == 1].sample(n_pos).index
+    
+    # Generate dataset
+    X_out = pd.concat([X_full.loc[neg_idx], X_full.loc[pos_idx]])
+    y_out = pd.concat([dat.loc[neg_idx], dat.loc[pos_idx]]).annotation
+
+    return y_out, X_out
+
+
+def iteration(args):
+    i = args[0]
+    sparsity = args[1]
+    logging.info(f'iteration {i}, sparsity {sparsity}: random')
+    
+    y, X = balance_data(X_full, df, sparsity) 
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, 
+                                                        random_state=221)
+
+    y_train.index = X_train.index
+    y_test.index = X_test.index
+
     out = []
     y_train_idx = pd.Series(shuffle(y_train.index))
-    stepsize = 100
+
+    # Calculate the stepsize to produce 30 steps
+    n_steps = 100
+    max_samples = X_train.shape[0]
+    stepsize = max_samples // n_steps
     ## Random annotation for increasing sample sizes
-    for s in range(10, 6000, stepsize):
+    for s in range(stepsize, max_samples, stepsize):
         annot_idx = y_train_idx.head(s)
 
-        y = y_train.loc[annot_idx]
-        X = X_train.loc[annot_idx]
+        temp_y = y_train.loc[annot_idx]
+        temp_X = X_train.loc[annot_idx]
         
         try:
-            clf.fit(X, y)
+            clf.fit(temp_X, temp_y)
             y_hat = clf.predict(X_test)
-            res = [s] + get_metrics(y_test, y_hat) + ['random_learner'] + [i]
+            res = ([s] + get_metrics(y_test, y_hat) + ['random_learner'] + [i] +
+                   [max_samples] + [sparsity])
         except Exception as e:
-            logging.error(f'RL {s}: {e}')
-            res = [s] + [0]*3 + ['random_learner'] + [i]
+            logging.error(f'RL {s}, {i}, {sparsity}: {e}')
+            res = ([s] + [0]*3 + ['random_learner'] + [i] + [max_samples] + 
+                   [sparsity])
      
         out.append(res)
 
-    annot_idx = y_train_idx.head(10)
-
+    annot_idx = y_train_idx.head(stepsize)
     ## active learning annotation for increasing sample sizes
-    print(f'iteration {i}: active annotation')
+    logging.info(f'iteration {i}, sparsity {sparsity}: active')
     for c in itertools.count():
-
         # Get annotated data (as chosen in last iteration)
-        y = y_train.loc[annot_idx]
-        X = X_train.loc[annot_idx]
-        
+        temp_y = y_train.loc[annot_idx]
+        temp_X = X_train.loc[annot_idx]
+
         try:
-            clf.fit(X, y)
+            clf.fit(temp_X, temp_y)
 
             # Get the scores for this iteration
             y_hat = clf.predict(X_test)
 
-            res = [len(annot_idx)] + get_metrics(y_test, y_hat) + ['active_learner'] + [i]
+            res = ([len(annot_idx)] + get_metrics(y_test, y_hat) + 
+                   ['active_learner'] + [i] + [max_samples] + [sparsity])
 
             # Get targets for next annotation step
             ## query least certain instances in the training set
@@ -119,17 +164,19 @@ def iteration(i):
             dist_from_05 = (y_hat_al - 0.5)**2
             most_uncertain = dist_from_05.nsmallest(n=stepsize)
             annot_idx = annot_idx.append(pd.Series(most_uncertain.index))
-      
+
+     
         except Exception as e:
             # Model failed 0 on the metrics and add random data
             m = c * stepsize
-            logging.error(f'AL {m}: {e}')
-            res = [len(annot_idx)] + [0]*3 + ['active_learner'] + [i]
-            annot_idx = y_train_idx.head((10+m+stepsize))
+            logging.error(f'AL {m}, {i}, {sparsity}: {e}')
+            res = ([len(annot_idx)] + [0]*3 + ['active_learner'] + [i] + 
+                   [max_samples] + [sparsity])
+            annot_idx = y_train_idx.head((stepsize+m+stepsize))
 
         out.append(res)
 
-        if len(annot_idx) > 6000:
+        if len(annot_idx) >= max_samples:
             break
  
     return out
@@ -140,6 +187,36 @@ def tuning_iteration(clf):
     y_hat = clf.predict(X_test)
     res = get_metrics(y_test, y_hat) + [clf.__repr__()]
     return res
+
+
+def process_text(text):
+    '''
+    Preprocess text by:
+        - Stemming
+        - Removing special characters
+
+    Args
+    text: str
+
+    Returns:
+    Preprocessed tokens as list
+    '''
+    re_url = re.compile(('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%'
+                         '[0-9a-fA-F][0-9a-fA-F]))+'))
+    re_remove = re.compile('[^A-Za-z0-9\w ]')
+    re_excs_space = re.compile('\s+')
+
+    # Remove urls:
+    text = re_url.sub(' ', text)
+    # Remove special characters
+    text = re_remove.sub(' ', text)
+    # Remove excess space
+    text = re_excs_space.sub(' ', text)
+
+    text = text.lower()
+    tokens = text.split()
+    out = [stemmer(t) for t in tokens]
+    return out
 
 if __name__ == "__main__":
 
@@ -182,13 +259,13 @@ if __name__ == "__main__":
 
     # Load nlp pipeline
     logging.info('Loading nlp pipeline...')
-    nlp = spacy.load('en')
+    stemmer = Stemmer.Stemmer('german').stemWord 
      
     # Generate bag of words representation
           
     ## First pass to generate uni and bigram dictionaries
     logging.info('Generating dictionaries...')
-    unigrams, bigrams = make_dictionaries(df.text, nlp)
+    unigrams, bigrams = make_dictionaries(df.text, process_text)
 
     ## Filter vocabularies
     unigrams.filter_extremes(no_below=2, no_above=0.2, keep_n=None)
@@ -196,7 +273,7 @@ if __name__ == "__main__":
 
     ### Create document term matrices for uni and bigrams
     logging.info('Generating uni and bigram features...')
-    grams = [make_ngrams(d, nlp, unigrams, bigrams) for d in df.text]
+    grams = [make_ngrams(d, process_text, unigrams, bigrams) for d in df.text]
     unigram_features = matutils.corpus2dense([x[0] for x 
                                             in grams],
                                             num_terms=len(unigrams)).transpose()
@@ -204,18 +281,12 @@ if __name__ == "__main__":
                                            in grams],
                                            num_terms=len(bigrams)).transpose()
 
-    X_full = pd.concat([pd.DataFrame(unigram_features),                      
-                        pd.DataFrame(bigram_features)], axis=1)
-    #X_full = pd.DataFrame(unigram_features)
+    #X_full = pd.concat([pd.DataFrame(unigram_features),                      
+    #                    pd.DataFrame(bigram_features)], axis=1)
+    X_full = pd.DataFrame(unigram_features)
+    X_full.index = df.index
     
-    ### Get test data
-    X_train, X_test, y_train, y_test = train_test_split(X_full, df.annotation, 
-                                                        test_size=0.33, 
-                                                        random_state=221)
-    y_train.index = X_train.index
-    y_test.index = X_test.index
-
-
+    
 
     # Tuning
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -253,8 +324,10 @@ if __name__ == "__main__":
     clf = SGDClassifier(penalty='elasticnet', loss='log', alpha=0.0001, 
                         l1_ratio=0.77, random_state=26661)
 
-    pool = Pool(10)
-    outputs = pool.map(iteration, np.arange(10))
+    pool = Pool(12)
+    params = list(itertools.product(np.arange(50), [0.005, 0.01, 0.05, 0.1, 
+                                                     0.3, 0.5]))
+    outputs = pool.map(iteration, params)
     pool.close()
 
     output = []
@@ -262,8 +335,9 @@ if __name__ == "__main__":
         output.extend(o)
 
 
-    with open('../data/active_random_sgd_log_all_data.csv', 'w') as outfile:
+    with open('../data/active_random_w_sparsity.csv', 'w') as outfile:
         writer = csv.writer(outfile, delimiter=',')
-        writer.writerow(['train_size','precision','recall','f1', 'clf', 'iteration'])
+        writer.writerow(['train_size','precision','recall','f1', 'clf', 
+                         'iteration', 'max_samples', 'sparsity'])
         for s in output:
             writer.writerow(s)
