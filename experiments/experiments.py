a3b1238891620a7375666a36d2d250815b547339
flinder <fridolin.linder@gmail.com>
2017-08-07 11:17:17 +0200

    evaluate boolean query on test not train set

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -707,8 +707,6 @@ if __name__ == "__main__":
     ts.to_latex('../paper/tables/top_terms.tex')
 
 
-
-
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # Boolean vs clf experiment
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -738,8 +736,8 @@ if __name__ == "__main__":
         for iteration in range(1, 201):
 
             words = np.random.choice(best_features, iteration, p=probs)
-            prediction = dtm_normalized[words].sum(axis=1) > 0
-            scores = get_metrics(df['annotation'], prediction)
+            prediction = X_test[words].sum(axis=1) > 0
+            scores = get_metrics(y_test, prediction)
             for i, measure in enumerate(['precision', 'recall', 'f1']):
                 output['replication'].append(replication)
                 output['iteration'].append(iteration)
